e09151a6e606f9db498ccd4bc69372fe034b192e
Harsh Sinha <harsh.sinha18@gmail.com>
2013-01-27 12:09:03 -0500

    Adding interface for instruction decoders and decoder factory

---
+++ bytecast-amd64/src/com/bytecast/amd64/internal/api/parser/IInstructionDecoder.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bytecast.amd64.internal.api.parser;
+
+import com.bytecast.amd64.api.instruction.IInstruction;
+
+/**
+ *
+ * @author harsh
+ */
+public interface IInstructionDecoder {
+    
+    IInstruction decodeInstruction(String instructionbytes);
+}
