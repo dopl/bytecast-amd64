52831aa9ef3e7c2f1f961af4a0bb154b29fa01f6
Haiyang Pan <dlut.phy@gmail.com>
2013-02-16 16:18:03 -0500

    update to the new folder structure and the code of JCC decoder

--- bytecast-amd64/src/edu/syr/bytecast/amd64/impl/decoder/JCCInstructionDecoder.java
+++ bytecast-amd64/src/edu/syr/bytecast/amd64/impl/decoder/JCCInstructionDecoder.java
@@ -9,6 +9,7 @@ import edu.syr.bytecast.amd64.api.instruction.IInstruction;
 import edu.syr.bytecast.amd64.impl.instruction.AMD64Instruction;
 import edu.syr.bytecast.amd64.impl.instruction.operand.OperandMemoryAddress;
 import edu.syr.bytecast.amd64.internal.api.parser.IInstructionDecoder;
+import edu.syr.bytecast.amd64.util.DecoderUtil;
 import java.util.List;
 /**
  *
@@ -16,69 +17,99 @@ import java.util.List;
  */
 public class JCCInstructionDecoder implements IInstructionDecoder {
     
+  @Override
+  public IInstruction decodeInstruction(Long instructionMemAddress, List<Byte> instructionbytes) {
+    IInstruction ret = null;
+    byte key_byte = instructionbytes.get(0).byteValue();
+    
+    if((key_byte & 0xF0) == 0x70) {
+      // 8 offset
+      ret = getInstruction((byte)(key_byte & 0x0F));
+    } else if(key_byte == 0x0F) {
+      key_byte = instructionbytes.get(1).byteValue();
+      if((key_byte & 0xF0) == 0x80) {
+        // 16 offset or 32 offset
+        ret = getInstruction((byte)(key_byte & 0x0F));
+      } else {
+        throw new IllegalArgumentException("Incorrect form for JCC instruction");
+      }
+    } else {
+      throw new IllegalArgumentException("Incorrect form for JCC instruction");
+    }
+        
+    // need add the method to parse the operands
+    decodeOperands(ret, instructionMemAddress, instructionbytes);
+
+    return ret;
+  }
+  
+    
   private void decodeOperands(IInstruction instruction, Long instructionMemAddress, List<Byte> instructionbytes) {
     byte tmp = instructionbytes.get(0).byteValue();
+    Long ltargetMemAddr = null;
     if(tmp != 0x0F) {
       // 8 offset JCC
       if(instructionbytes.size() != 2) {
        throw new IllegalArgumentException("For 8 offset JCC, the length of instruction should be 2 bytes"); 
       }
-      
-      Long targetMemAddr = instructionMemAddress + instructionbytes.size() + instructionbytes.get(1);
-      OperandMemoryAddress operandMemAddr = new OperandMemoryAddress(targetMemAddr); 
-      instruction.addOperand(operandMemAddr);
+      long targetMemAddr = instructionMemAddress.longValue() + instructionbytes.size() + instructionbytes.get(1).byteValue();
+      ltargetMemAddr = (long)targetMemAddr;
     } else {
-      // 16 offset JCC or larger
-      throw new UnsupportedOperationException("Not support JCC that larger than 8 offset yet.");
+      // 16 offset JCC or 32 offset JCC
+      //throw new UnsupportedOperationException("Not support JCC that larger than 8 offset yet.");
+      if(instructionbytes.size() == 4) {
+        Long offset = DecoderUtil.ByteConcatenator(instructionbytes, 2);
+        long targetMemAddr = instructionMemAddress.longValue() + instructionbytes.size() + offset.shortValue();
+        ltargetMemAddr = (long)targetMemAddr;
+      } else if(instructionbytes.size() == 6) {
+        Long offset = DecoderUtil.ByteConcatenator(instructionbytes, 4);
+        long targetMemAddr = instructionMemAddress.longValue() + instructionbytes.size() + offset.intValue();
+        ltargetMemAddr = (long)targetMemAddr;
+      } else {
+        throw new IllegalArgumentException("For 16 or 32 offset JCC, the length of instruction should be either 4 or 6 bytes"); 
+      }
     }
+    OperandMemoryAddress operandMemAddr = new OperandMemoryAddress(ltargetMemAddr); 
+    instruction.addOperand(operandMemAddr);
   }
-    
-  @Override
-  public IInstruction decodeInstruction(Long instructionMemAddress, List<Byte> instructionbytes) {
-    IInstruction ret = null;
-    byte tmp = instructionbytes.get(0).byteValue();
-    if((tmp & 0xF0) == 0x70) {
-      switch(tmp & 0x0F) {
-        case 0x02:
-          ret = new AMD64Instruction(InstructionType.JB);
-          break;
-        case 0x03:
-          ret = new AMD64Instruction(InstructionType.JAE);
-          break;
-        case 0x04:
-          ret = new AMD64Instruction(InstructionType.JE);
-          break;
-        case 0x05:
-          ret = new AMD64Instruction(InstructionType.JNE);
-          break;
-        default:
-          break;
-      }
-    } else if(tmp == 0x0F) {
-      tmp = instructionbytes.get(1).byteValue();
-      if((tmp & 0xF0) == 0x80) {
-        switch(tmp & 0x0F) {
-          case 0x02:
-            ret = new AMD64Instruction(InstructionType.JB);
-            break;
-          case 0x03:
-            ret = new AMD64Instruction(InstructionType.JAE);
-            break;
-          case 0x04:
-            ret = new AMD64Instruction(InstructionType.JE);
-            break;
-          case 0x05:
-            ret = new AMD64Instruction(InstructionType.JNE);
-            break;
-          default:
-            break;
-        }
-      }
+  
+  private IInstruction getInstruction(byte tmp) {
+    switch(tmp) {
+      case 0x00:
+        return new AMD64Instruction(InstructionType.JO);
+      case 0x01:
+        return new AMD64Instruction(InstructionType.JNO);
+      case 0x02:
+        return new AMD64Instruction(InstructionType.JB);
+      case 0x03:
+        return new AMD64Instruction(InstructionType.JAE);
+      case 0x04:
+        return new AMD64Instruction(InstructionType.JE);
+      case 0x05:
+        return new AMD64Instruction(InstructionType.JNE);
+      case 0x06:
+        return new AMD64Instruction(InstructionType.JBE);
+      case 0x07:
+        return new AMD64Instruction(InstructionType.JA);
+      case 0x08:
+        return new AMD64Instruction(InstructionType.JS);
+      case 0x09:
+        return new AMD64Instruction(InstructionType.JNS);
+      case 0x0A:
+        return new AMD64Instruction(InstructionType.JP);
+      case 0x0B:
+        return new AMD64Instruction(InstructionType.JNP);
+      case 0x0C:
+        return new AMD64Instruction(InstructionType.JL);
+      case 0x0D:
+        return new AMD64Instruction(InstructionType.JGE);
+      case 0x0E:
+        return new AMD64Instruction(InstructionType.JLE);
+      case 0x0F:
+        return new AMD64Instruction(InstructionType.JG);
+      default:
+        return null;
     }
-    // need add the method to parse the operands
-    decodeOperands(ret, instructionMemAddress, instructionbytes);
-
-    return ret;
   }
     
 }
