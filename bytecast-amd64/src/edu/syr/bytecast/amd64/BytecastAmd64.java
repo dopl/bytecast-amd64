a1114e2fefa8f79b11f32d46e8d6795023435d97
Sheng Wang <newton449@gmail.com>
2013-03-23 18:10:34 -0400

    Merge

--- bytecast-amd64/src/edu/syr/bytecast/amd64/BytecastAmd64.java
+++ bytecast-amd64/src/edu/syr/bytecast/amd64/BytecastAmd64.java
@@ -23,6 +23,7 @@ import edu.syr.bytecast.amd64.api.constants.IBytecastAMD64;
 import edu.syr.bytecast.amd64.api.instruction.IInstruction;
 import edu.syr.bytecast.amd64.api.output.IExecutableFile;
 import edu.syr.bytecast.amd64.api.output.ISection;
+import edu.syr.bytecast.amd64.api.output.MemoryInstructionPair;
 import edu.syr.bytecast.amd64.impl.dictionary.AMD64Dictionary;
 import edu.syr.bytecast.amd64.impl.lexer.AMD64InstructionLexer;
 import edu.syr.bytecast.amd64.impl.output.AMD64ExecutableFile;
@@ -94,8 +95,8 @@ public class BytecastAmd64 implements IBytecastAMD64{
         int index=0;
            for(ExeObjSegment segment : segments)
            {
-               Map<Long,IInstruction> instructions 
-                     = lexer.convertInstructionBytesToObjects(segment.getStartAddress(), segment.getBytes());
+               List<MemoryInstructionPair> instructions 
+                     = null; //lexer.convertInstructionBytesToObjects(segment.getStartAddress(), segment.getBytes());
                ISection section = new AMD64Section(instructions,segment.getStartAddress(),entryPointIndex==index);
                sections.add(section);
                ++index;
